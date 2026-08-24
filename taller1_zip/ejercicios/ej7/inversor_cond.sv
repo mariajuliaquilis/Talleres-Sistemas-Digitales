a1a724035b3a6405a7968c06b28918c91a892cbb
mariajuliaquilis <quilismariajulia@hotmail.com>
2026-08-24 20:02:31 -0300

    Subo el ej 7

--- taller1_zip/ejercicios/ej7/inversor_cond.sv
+++ taller1_zip/ejercicios/ej7/inversor_cond.sv
@@ -3,5 +3,11 @@ module inversor_cond (
     input  logic       ctrl,
     output logic [3:0] y
 );
-  // completar
+  
+  assign y = ctrl ? ~x : x; //No hace falta hacer esto para cada bit.
+  //assign y[0] = ctrl ? ~x[0] : x[0];
+  //assign y[1] = ctrl ? ~x[1] : x[1];
+  //assign y[2] = ctrl ? ~x[2] : x[2];
+  //assign y[3] = ctrl ? ~x[3] : x[3];
+
 endmodule
---
+++ taller1_zip/ejercicios/ej7/obj_dir/Vsim_top__ALL.d
@@ -0,0 +1,11 @@
+Vsim_top__ALL.o: Vsim_top__ALL.cpp Vsim_top.cpp Vsim_top__pch.h \
+ /usr/local/share/verilator/include/verilated.h \
+ /usr/local/share/verilator/include/verilated_config.h \
+ /usr/local/share/verilator/include/verilatedos.h \
+ /usr/local/share/verilator/include/verilated_types.h \
+ /usr/local/share/verilator/include/verilated_funcs.h Vsim_top__Syms.h \
+ Vsim_top.h Vsim_top___024root.h \
+ /usr/local/share/verilator/include/verilated_timing.h \
+ /usr/local/share/verilator/include/verilated.h Vsim_top___024root__0.cpp \
+ Vsim_top__main.cpp Vsim_top___024root__Slow.cpp \
+ Vsim_top___024root__0__Slow.cpp Vsim_top__Syms__Slow.cpp
